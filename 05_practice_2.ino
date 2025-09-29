93e1301da10ed21474628e9c495c0a30561ceb3f
kmu-sehui <sehui618@gmail.com>
2025-09-30 07:06:17 +0900

    Create 05_practice_2.ino

---
+++ 05_practice_2.ino
@@ -0,0 +1,23 @@
+void setup() {
+  pinMode(7, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(7, LOW);
+  delay(1000);
+
+
+  for (int i = 0; i < 5; i++) {
+    digitalWrite(7, HIGH);  
+    delay(100);
+    digitalWrite(7, LOW);   
+    delay(100);
+  }
+
+
+  digitalWrite(7, HIGH);
+
+  
+  while (1) {
+  }
+}
